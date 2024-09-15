06b5242b379f6f464199bd58d4e192e64b0aba28
Sacha Wheeler <sacha@sachawheeler.com>
2024-09-15 20:18:55 +0100

    new set up

--- mount_happy.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-#sshfs happy@happy.local:/Users/happy happy_share
-sshfs -o allow_other happy@happy.local:/Volumes/moshpit /moshpit
-# sshfs -o allow_other,default_permissions  happy@happy.local:/Volumes/moshpit/youtube /media/sacha/youtube
-
